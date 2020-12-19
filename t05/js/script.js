82c8b0788191d9be6a2b5500f115a1a5261f05b5
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2020-12-19 23:26:16 +0200

    edited task

--- t05/js/script.js
+++ t05/js/script.js
@@ -1,6 +1,32 @@
-dragElement(document.getElementById("stone"));
+let container = document.querySelector('.container');
+let stone = document.querySelector('.stone');
 
-function dragElement(el) {
-    let p1 = 0, p2 = 0, p3 = 0, p4 = 0;
-    console.log(el);
-}
+let state = { distX: 0, distY: 0 };
+
+function onDown(e) {
+  e.preventDefault();
+  let evt = e.type === 'touchstart' ? e.changedTouches[0] : e;
+  
+  state.distX = Math.abs(stone.offsetLeft - evt.clientX);
+  state.distY = Math.abs(stone.offsetTop - evt.clientY);
+  stone.style.pointerEvents = 'none';
+};
+
+function onUp(e) {
+    stone.style.pointerEvents = 'initial';
+};
+
+function onMove(e) {
+  if (stone.style.pointerEvents === 'none') {
+    var evt = e.type === 'touchmove' ? e.changedTouches[0] : e;
+    stone.style.left = `${evt.clientX - state.distX}px`;
+    stone.style.top = `${evt.clientY - state.distY}px`;
+  };
+};
+
+stone.addEventListener('mousedown', onDown);
+container.addEventListener('mousemove', onMove);
+container.addEventListener('mouseup', onUp);
+stone.addEventListener('touchstart', onDown);
+container.addEventListener('touchmove', onMove);
+container.addEventListener('touchend', onUp);
